3c0667385171eb3637a68b00a7fe247faf5c7948
edmundo fuentesrivera <edmundo.fuentesrivera@frappurecords.com>
2026-06-02 17:16:24 -0500

    fix: remove migrate from build, use pre-deploy

--- backend/build.sh
+++ backend/build.sh
@@ -4,8 +4,8 @@ set -e
 echo "==> Instalando dependencias..."
 pip install -r requirements.txt
 
-echo "==> Ejecutando migraciones..."
-python manage.py migrate --noinput
+#echo "==> Ejecutando migraciones..."
+#python manage.py migrate --noinput
 
 echo "==> Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
